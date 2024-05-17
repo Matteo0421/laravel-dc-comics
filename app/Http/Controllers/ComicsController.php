e339eb613db6e51fa9628f03fb881290a325ba20
Matteo0421 <matteo.carola99@gmail.com>
2024-05-18 01:10:41 +0200

    [fix]bug

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -37,19 +37,28 @@ class ComicsController extends Controller
         $form_data = $request->all();
 
 
-        $new_comic = new Comic();
-        $new_comic->title = $form_data['title'];
-        $new_comic->slug = Helper::generateSlug($new_comic->title, new Comic());
-        $new_comic->description = $form_data['description'];
-        $new_comic->thumb = $form_data['thumb'];
-        $new_comic->price = $form_data['price'];
-        $new_comic->series = $form_data['series'];
-        $new_comic->sale_date = $form_data['sale_date'];
-        $new_comic->type = $form_data['type'];
-        $new_comic->artists = json_encode(explode(',', $form_data['artists']));
-        $new_comic->writers = json_encode(explode(',', $form_data['writers']));
+        // $new_comic = new Comic();
+        // $new_comic->title = $form_data['title'];
+        // $new_comic->slug = Helper::generateSlug($new_comic->title, new Comic());
+        // $new_comic->description = $form_data['description'];
+        // $new_comic->thumb = $form_data['thumb'];
+        // $new_comic->price = $form_data['price'];
+        // $new_comic->series = $form_data['series'];
+        // $new_comic->sale_date = $form_data['sale_date'];
+        // $new_comic->type = $form_data['type'];
+        // $new_comic->artists = json_encode(explode(',', $form_data['artists']));
+        // $new_comic->writers = json_encode(explode(',', $form_data['writers']));
 
+        // $new_comic->save();
+
+
+        $new_comic = new Comic();
+        $new_comic->slug = Helper::generateSlug($new_comic->title, new Comic());
+          $new_comic->artists = json_encode(explode(',', $form_data['artists']));
+        $new_comic->writers = json_encode(explode(',', $form_data['writers']));
+        $new_comic->fill($form_data);
         $new_comic->save();
+
 
         return redirect()->route('comics.show',$new_comic );
 
@@ -86,6 +95,7 @@ class ComicsController extends Controller
         }else{
             $form_data['slug'] = Helper::generateSlug($form_data['title'], new Comic());
         }
+
 
         $comic->update($form_data);
 
